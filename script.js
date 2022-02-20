9146425f54893262fd564dcdebe38325cad3ba5c
tahsin_dev <tahsinahmed52225@gmail.com>
2022-02-20 15:42:01 +0600

    Done

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("HI");
+$(function () {
+    $.ajax({
+        url: 'https://api.my-ip.io/ip.json',
+        method: 'GET'
+    })
+        .done(function (data) {
+            $('#ip').text(data.ip);
+            if (data.ip == '203.76.222.138') {
+                $('#status').addClass('btn-success');
+                $('#status').addClass('btn-primary');
+            } else {
+                $('#status').addClass('btn-danger');
+                $('#status').addClass('btn-primary');
+            }
+
+        })
+        .fail(function () {
+            // Handle failure
+        });
+});
